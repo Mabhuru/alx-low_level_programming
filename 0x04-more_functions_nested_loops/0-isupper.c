8bcf95bef9fbb659ff8c9d619ef33349273f6444
Mabhuru <mabhuru7@gmail.com>
2023-06-22 12:25:42 -0700

    ndamama

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#define main_h
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+*main - Entry point
+*
+*Description: checks for uppercase letters
+*
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+char c = 'A';
+
+if (c == 'A')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
+}
