a88fc5bc35acfdd5a64fcf7bcce824969007e604
Mabhuru <mabhuru7@gmail.com>
2023-06-27 06:09:56 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 /**
  * updateValueTo98 - Updates the value pointed to by ptr to 98.
  * @ptr: Pointer to an int.
@@ -7,5 +7,5 @@ void reset_to_98(int *n);
  *              to 98.
  */
 {
-	*ptr = 98;
+	*n = 98;
 }
