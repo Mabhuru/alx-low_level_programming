c1a912f0ecf4faa5ee1d7d3f000e466dd4771274
Mabhuru <mabhuru7@gmail.com>
2023-06-29 06:21:39 -0700

    caps

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,22 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	/*check the first index for capital*/
-	if (s[i] >= 'a' && s[i] <= 'Z')
+	/* check first index for capital */
+	if (s[i] >= 'a' && s[i] <= 'z')
 		s[i] = s[i] - 'a' + 'A';
 	i++;
 
-	while (s[i] != '\0') /*iterate thru string*/
+	while (s[i] != '\0') /* iterate through string */
 	{
-		/*if lowercase and prior character is separator, make capital*/
-		if ((s[i] >= 'a' && s[i] <= 'Z'
-				&& (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
-					s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
-					s[i = 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
-					s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t' ||
-					s[i - 1] == '\n')))
-					s[i] = s[i] - 'a' + 'A';
+
+		/* if lowercase and prior char is separator, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
 		i++;
 	}
 
